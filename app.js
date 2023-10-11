a222c7ce1a01990d9636558520f67487a206c0ca
Pedro Lima <tsc.pedrolima@gmail.com>
2023-10-11 18:37:20 -0300

    feat: add route to get news

--- app.js
+++
@@ -1,6 +0,0 @@
-const express = require("express")
-const app = express()
-
-app.listen(4001, () => {
-    console.log("Server running in port: 4001")
-})
---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT || 4001
+const newsRouter = require("./news")
+
+app.use("/news", newsRouter)
+
+app.listen(PORT, () => {
+    console.log("Server running in port: " + PORT)
+})
