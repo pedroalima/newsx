0a1070e17b2e21dc026ca07b00393483fcad244f
Pedro Lima <tsc.pedrolima@gmail.com>
2023-10-12 21:22:05 -0300

    feat: add put route

--- news.js
+++ news.js
@@ -22,16 +22,44 @@ const news = [
     },
 ]
 
+const getIndexById = (id, elementList) => {
+    return elementList.find(element => element.id === Number(id))
+}
+
+const updateElement = (id, queryArguments, elementList) => {
+    const elementIndex = getIndexById(id, elementList);
+    if (elementIndex === -1) {
+      throw new Error('updateElement must be called with a valid id parameter');
+    }
+    if (queryArguments.id) {
+      queryArguments.id = Number(queryArguments.id);
+    }
+    Object.assign(elementList[elementIndex], queryArguments);
+    return elementList[elementIndex];
+};
+
 // Get all news
 newsRouter.get("/", (req, res) => {
     res.send(news)
 })
 
-// Get a news by ID
+// Get news by ID
 newsRouter.get("/:id", (req, res) => {
     const newsId = news.find(notice => notice.id === Number(req.params.id))
 
     newsId ? res.send(newsId) : res.status(404).send("News not found")
+})
+
+// Update news
+newsRouter.put("/:id", (req, res) => {
+    const newsIndex = news.findIndex(notice => notice.id === Number(req.params.id))
+    
+    if (newsIndex !== -1) {
+        updateElement(req.params.id, req.query, news)
+        res.send(news[newsIndex])
+    } else {
+        res.status(404).send()
+    }
 })
 
 module.exports = newsRouter
