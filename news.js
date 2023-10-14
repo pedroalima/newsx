71d3731416874ba5a3221f71619e69343ddf6a15
Pedro Lima <tsc.pedrolima@gmail.com>
2023-10-14 19:27:27 -0300

    feat: add put route

--- news.js
+++ news.js
@@ -5,8 +5,12 @@ const newsRouter = express.Router()
 const news = []
 
 // Functions utils
-const getId = (elementList, id) => {
-    return elementList.find(element => element.id === Number(id))
+const getId = (id, elementList) => {
+    return elementList.find(element => element.id === id)
+}
+
+const getIndex = (id, elementList) => {
+    return elementList.findIndex(element => element.id === id)
 }
 
 // Get all news
@@ -16,7 +20,7 @@ newsRouter.get("/", (req, res) => {
 
 // Get news by ID
 newsRouter.get("/:id", (req, res) => {
-    const newsId = getId(news, req.params.id)
+    const newsId = getId(req.params.id, news)
     newsId ? res.send(newsId) : res.status(404).send("News not found")
 })
 
@@ -33,9 +37,23 @@ newsRouter.post("/", (req, res) => {
     res.status(201).json(notice)
 })
 
-// // Update news
-// newsRouter.put("/:id", (req, res, next) => {
-    
-// })
+// Update news
+newsRouter.put("/:id", (req, res, next) => {
+    const { id } = req.params
+    const noticeIndex = getIndex(id, news)
+    const { lead_image, title, date } = req.body
+    if (noticeIndex !== -1) {
+        newNotice = {
+            id,
+            lead_image,
+            title,
+            date
+        }
+        news[noticeIndex] = newNotice
+        res.json(news[noticeIndex])
+    } else {
+        res.status(404).send("Item not found")
+    }
+})
 
 module.exports = newsRouter
