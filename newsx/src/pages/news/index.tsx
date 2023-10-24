d5a65ac3a814972131aa88c19dfc31f20dec9e80
Pedro Lima <tsc.pedrolima@gmail.com>
2023-10-23 21:53:42 -0300

    feat: add get-news function

---
+++ newsx/src/pages/news/index.tsx
@@ -0,0 +1,11 @@
+import * as M from "@mui/material"
+import { useState } from "react";
+
+export const News = () => {
+    const theme = M.useTheme();
+	const [allNews, setAllNews] = useState([]);
+
+    return (
+        
+    )
+}
