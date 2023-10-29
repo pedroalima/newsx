3731d6c4e515f72c10804c9c7a703cdf0ccfc590
Pedro Lima <tsc.pedrolima@gmail.com>
2023-10-28 21:34:30 -0300

    style: layout form

--- newsx/src/shared/components/create-news/index.tsx
+++ newsx/src/shared/components/create-news/index.tsx
@@ -1,17 +1,59 @@
 import * as M from "@mui/material";
+import * as S from "./style";
+import { FormEvent, useRef } from "react";
+import axios from "axios";
+
+interface NewsType {
+	lead_image: {value: string}
+	title: {value: string}
+	content: {value: string}
+	date: {value: string}
+
+}
 
 export const CreateNews = () => {
+	const ref = useRef();
+	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		const news: NewsType = ref.current;
+
+		try {
+			const res = await axios.post("http://localhost:4001/news", {
+				leade_image: news.lead_image.value,
+				title: news.title.value,
+				content: news.content.value,
+				date: news.date.value
+			});
+
+			console.log(res.data);
+		} catch (error) {
+			console.log(error);
+		}
+
+		news.lead_image.value = "";
+		news.title.value = "";
+		news.content.value = "";
+		news.date.value = "";
+
+		console.log("Submit");
+	};
+
 	return (
-		<M.Box>
-			<form action="" method="post">
+		<M.Box height="60vh">
+			<S.Form onSubmit={handleSubmit}>
+				<M.TextField name="lead_image" label="Lead image" variant="standard" size="small" />
+				<M.TextField name="title" label="Title" variant="standard" size="small" />
 				<M.TextField
-					id="outlined-multiline-flexible"
-					label="Multiline"
+					name="content"
+					label="Content"
 					multiline
-					maxRows={4}
+					fullWidth
+					maxRows={9}
 				/>
-				<M.Button>Confirm</M.Button>
-			</form>
+				<S.Input name="date" type="date"  />
+				<M.Button type="submit" color="secondary" variant="contained">Confirm</M.Button>
+			</S.Form>
 		</M.Box>
 	);
 };
---
+++ newsx/src/shared/components/create-news/style.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    gap: 1.5rem;
+`;
+
+export const Input = styled.input`
+    background:none;
+    color: #71627D;
+    border: 1px solid #BEBDBB;
+    padding: 0.3rem 1rem;
+    border-radius: 5px
+`;
